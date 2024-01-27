34b6325409fd579c6d2f832d626e4b15e16db7b8
Zagidin Selimov <seljmov@list.ru>
2024-01-27 20:49:13 +0300

    fix sh

--- src/Thesis.Requests.Server/docker-publish.cmd
+++ src/Thesis.Requests.Server/docker-publish.cmd
@@ -1,5 +1,5 @@
 @echo off
 dotnet publish -c Release
-cd bin/Release/net7.0/publish
+cd bin/Release/net8.0/publish
 docker buildx build --platform linux/amd64 -t seljmov/thesis-requests:amd64 .
 docker push seljmov/thesis-requests:amd64
